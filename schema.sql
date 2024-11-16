4cbacb2adfa87f376f5d7c3490c6920ca31e952f
Ludovic Aubert <ludo.aubert@gmail.com>
2024-11-16 16:12:10 +0100

    Create schema.sql
    WIP. To be refined

---
+++ schema.sql
@@ -0,0 +1,56 @@
+CREATE TABLE IF NOT EXISTS diagram(
+  iddiagram SERIAL PRIMARY KEY,
+  title VARCHAR(128)
+);
+
+CREATE TABLE IF NOT EXISTS box(
+  idbox SERIAL PRIMARY KEY,
+  title VARCHAR(128),
+  iddiagram INTEGER,
+  FOREIGN KEY (iddiagram) REFERENCES diagram(iddiagram)
+);
+
+CREATE TABLE IF NOT EXISTS field(
+  idfield SERIAL PRIMARY KEY,
+  name VARCHAR(128),
+  idbox INTEGER,
+  FOREIGN KEY (idbox) REFERENCES box(idbox)
+);
+
+CREATE TABLE IF NOT EXISTS link(
+  idlink SERIAL PRIMARY KEY,
+  idbox_from INTEGER,
+  idbox_to INTEGER
+  FOREIGN KEY (idbox_from) REFERENCES box(idbox),
+  FOREIGN KEY (idbox_to) REFERENCES box(idbox)
+);
+
+CREATE TABLE IF NOT EXISTS tag(
+  idtag SERIAL PRIMARY KEY,
+  type_code VARCHAR(128),
+  code VARCHAR(128)
+);
+
+CREATE TABLE rectangle(
+  idrectangle SERIAL PRIMARY KEY,
+  width INTEGER,
+  height INTEGER,
+  idbox INTEGER,
+  FOREIGN KEY (idbox) REFERENCES box(idbox)
+);
+
+CREATE TABLE translation(
+  idtranslation SERIAL PRIMARY KEY,
+  idrectangle INTEGER,
+  x INTEGER,
+  y INTEGER,
+  FOREIGN KEY (idrectangle) REFERENCES rectangle(idrectangle)
+);
+
+CREATE TABLE point(
+  idpoint SERIAL PRIMARY KEY,
+  idlink INTEGER,
+  x INTEGER NULL,
+  y INTEGER NULL,
+  FOREIGN KEY (idlink) REFERENCES link(idlink)
+);
