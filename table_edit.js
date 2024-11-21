d68fea0916ee4ded0cf0a547a544cc8eec80aa15
Ludovic Aubert <ludo.aubert@gmail.com>
2024-11-21 13:44:14 +0100

    Update table_edit.js

--- table_edit.js
+++ table_edit.js
@@ -5,23 +5,9 @@ import {mycontexts, contexts, resetContexts, setContexts, drawDiag, compute_link
 import {download} from "./iocomponent.js";
 import {getFileData} from "./iocomponent.js";
 
-export {db, init, mydata, data, resetData, setData, displayCurrent};
+export {db, init, setData, displayCurrent};
 
 const db = new PGlite();
-
-var mydata = sample_diagdata;
-
-function setData(mydata_)
-{
-	mydata = mydata_;
-	data = JSON.stringify(mydata);
-}
-
-var data=null;
-function resetData()
-{
-	data=null;
-}
 
 var currentBoxIndex = -1;
 var currentFieldIndex = -1;
