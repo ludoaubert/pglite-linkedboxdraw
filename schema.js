4169c6e7e6e8ceeb6d4c9ed6105facd4f40b3ce4
Ludovic Aubert <ludo.aubert@gmail.com>
2024-11-27 15:51:54 +0100

    Update schema.js

--- schema.js
+++ schema.js
@@ -65,6 +65,17 @@ CREATE TABLE IF NOT EXISTS message_tag(
   message TEXT
 );
 
+CREATE FUNCTION is_table_primary_key(_table_name TEXT, _id INTEGER) RETURNS BOOLEAN AS
+BEGIN
+  RETURN
+    EXISTS(SELECT * FROM tag WHERE _table_name='tag' AND _id=idtag) OR
+    EXISTS(SELECT * FROM message_tag WHERE _table_name='message_tag' AND _id=idmessage) OR
+    EXISTS(SELECT * FROM box WHERE _table_name='box' AND _id=idbox) OR
+    EXISTS(SELECT * FROM field WHERE _table_name='field' AND _id=idfield) OR
+    EXISTS(SELECT * FROM value WHERE _table_name='value' AND _id=idvalue) OR
+    EXISTS(SELECT * FROM link WHERE _table_name='link' AND _id=idlink);
+END
+
 CREATE TYPE source_table AS ENUM ('tag', 'message_tag');
 CREATE TYPE target_table AS ENUM ('box', 'field', 'value', 'link');
   
@@ -74,7 +85,9 @@ CREATE TABLE IF NOT EXISTS graph(
   from_key INTEGER,
   to_table target_table,
   to_key INTEGER,
-  UNIQUE(from_table, from_key, to_table, to_key)
+  UNIQUE(from_table, from_key, to_table, to_key),
+  CHECK(is_table_primary_key(from_table::text, from_key)),
+  CHECK(is_table_primary_key(to_table::text, to_key))
 );
 
 -- INSERT INTO graph(from_table, from_key, to_table, to_key) VALUES('message_tag',1,'box',1);
