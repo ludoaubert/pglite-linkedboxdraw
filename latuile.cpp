51a5e2ed956a5cfbfdfff6a1173495f6a0918570
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-03 12:32:56 +0100

    Update latuile.cpp

--- latuile.cpp
+++ latuile.cpp
@@ -410,8 +410,10 @@ Linux command to install eigen3 directory:
 Linux command to lookup eigen3 directory:
  sudo find / -type d -name "eigen3"
 
+git checkout 30b7a332da77d55576d6f15ab65b587192c6aabc
+
 To generate latuile.wasm and latuile.js:
-emcc latuile.cpp binpack.cpp compact_frame.cpp compact_rectangles.cpp fit_together.cpp KMeansRexCore.cpp MyRect.cpp optimize_rectangle_positions.cpp permutation.cpp stair_steps.cpp swap_rectangles.cpp WidgetContext.cpp FunctionTimer.cpp -o latuile.js -I/usr/include/eigen3 -Wno-c++11-narrowing -s EXPORTED_FUNCTIONS='["_latuile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s ALLOW_MEMORY_GROWTH=1  -s EXPORT_ES6=1 -s MODULARIZE=1 -s EXPORT_NAME="createLatuileModule"
+emcc latuile.cpp binpack.cpp compact_frame.cpp compact_rectangles.cpp fit_together.cpp KMeansRexCore.cpp MyRect.cpp optimize_rectangle_positions.cpp permutation.cpp stair_steps.cpp swap_rectangles.cpp WidgetContext.cpp FunctionTimer.cpp MPD_Arc.cpp -o latuile.js -I/usr/include/eigen3 -Wno-c++11-narrowing -s EXPORTED_FUNCTIONS='["_latuile"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s ALLOW_MEMORY_GROWTH=1  -s EXPORT_ES6=1 -s MODULARIZE=1 -s EXPORT_NAME="createLatuileModule"
 
 using cmake seemed more complicated.
 */
