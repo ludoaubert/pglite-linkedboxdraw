a6a6d7fb5d7a8d251320d1f46e21a1d6bf97ac6d
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-03 16:38:44 +0100

    Update diagdata.sql

--- diagdata.sql
+++ diagdata.sql
@@ -1,260 +1,86 @@
-{"documentTitle":"Jacket Gafa BlemePro","boxes":[
-{"title":"ChanmeBlemeproPec",
-"id":0,
-"fields":[
-	{"name":"teillebouRap","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"PouilledePotecaCaillerag",
-"id":1,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"reusta","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"Blemepro",
-"id":2,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"Peuclo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"name","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"phoneTel","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"donbi","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"TassepeTcheubi",
-"id":3,
-"fields":[
-	{"name":"telmor","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"meuf","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"guedinTeub","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"teillebouRap","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"guezmere","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"donbi","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"GuedraTeucheTipeu",
-"id":4,
-"fields":[
-	{"name":"tepo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"teillebouRap","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"TejeTiepeTigenToncarInsseGueusGuedinUc",
-"id":5,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yeuf","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"KeneVeugra","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"Lassedege",
-"id":6,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"al","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zessegon","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"Peuclo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yepe","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zeto","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"phoneTel","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"fechnoueuf",
-"id":7,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"gova","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"vessause","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"ZetiNoichNeuthuZincMeucaYoce",
-"id":8,
-"fields":[
-	{"name":"[lassedeg]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"[yecous]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zebe","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"zomblou",
-"id":9,
-"fields":[
-	{"name":"[zedouZicmus]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"[mifaOideOiteOe]","isPrimaryKey":true,"isForeignKey":false},
-	{"name":"[zessegonNeuthu]","isPrimaryKey":true,"isForeignKey":false},
-	{"name":"yeuf","isPrimaryKey":true,"isForeignKey":false}
-]},
-{"title":"lassedegLeurdit",
-"id":10,
-"fields":[
-	{"name":"[zomblou]{2}","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zenZincs","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"vessause","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yeuf","isPrimaryKey":false,"isForeignKey":true},
-	{"name":"oinje","isPrimaryKey":false,"isForeignKey":true}
-]},
-{"title":"youvoi",
-"id":11,
-"fields":[
-	{"name":"[yeufi]{2}","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zyva","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yepe","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zetyenchesZeyo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"nemobitKps","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"keuf","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"lacelles",
-"id":12,
-"fields":[
-	{"name":"keblo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"euf","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"mefu",
-"id":13,
-"fields":[
-	{"name":"cesu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zeberyep","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"youvoiUc","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"euf",
-"id":14,
-"fields":[
-	{"name":"lassedeg","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"id","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"fichateraToDeuspidrepo","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yecous","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"zedouZicmus",
-"id":15,
-"fields":[
-	{"name":"[updatesKecos]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"yeufiyechKeco","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"vessause",
-"id":16,
-"fields":[
-	{"name":"lassedeg","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"persistenceYeche","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"KeneVeugra",
-"id":17,
-"fields":[
-	{"name":"name","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"[symbolsIndices]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"kisdeAggregator","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"keusse",
-"id":18,
-"fields":[
-	{"name":"al","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"zeto","isPrimaryKey":false,"isForeignKey":false}
-]},
-{"title":"usage",
-"id":19,
-"fields":[
-	{"name":"[zomblouLeurdits]","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"{youvois}","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"fechnoueuf","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"lancebaVeucheYeuveZarbiZeu","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"Zeyos","isPrimaryKey":false,"isForeignKey":false},
-	{"name":"euf","isPrimaryKey":false,"isForeignKey":false}
-]}],
-"values":[
-{"box":"zedouZicmus","field":"yeufiyechKeco","value":"YOCLeregaLeurdiData"},
-{"box":"zedouZicmus","field":"yeufiyechKeco","value":"YOCLeustLeurdiData"},
-{"box":"zedouZicmus","field":"yeufiyechKeco","value":"YOCLeustStatisticsData"},
-{"box":"zedouZicmus","field":"yeufiyechKeco","value":"YOCReferentialData"},
-{"box":"mefu","field":"zeberyep","value":"eth0"},
-{"box":"mefu","field":"zeberyep","value":"eth1"},
-{"box":"TassepeTcheubi","field":"donbi","value":"oufin"},
-{"box":"TassepeTcheubi","field":"donbi","value":"oufout"},
-{"box":"TassepeTcheubi","field":"donbi","value":"yecou"},
-{"box":"euf","field":"yecous","value":"mifa"},
-{"box":"euf","field":"yecous","value":"zen-oim"},
-{"box":"euf","field":"yecous","value":"zen-neujeuyenche"},
-{"box":"euf","field":"yecous","value":"zen-starcotarb"},
-{"box":"euf","field":"yecous","value":"ss-zen"},
-{"box":"euf","field":"yecous","value":"ss-zessegon"},
-{"box":"euf","field":"yecous","value":"zessegon"},
-{"box":"euf","field":"yecous","value":"zen"},
-{"box":"Blemepro","field":"donbi","value":"oufin"},
-{"box":"Blemepro","field":"donbi","value":"oufout"},
-{"box":"Blemepro","field":"donbi","value":"yecou"},
-{"box":"youvoi","field":"zyva","value":"realmefu"},
-{"box":"youvoi","field":"zyva","value":"conflated"},
-{"box":"youvoi","field":"zyva","value":"zessegon"},
-{"box":"youvoi","field":"keuf","value":"lopesa"},
-{"box":"youvoi","field":"keuf","value":"zebermef"},
-{"box":"zedouZicmus","field":"[yenchesKecos]","value":"Yepe_Bid"},
-{"box":"zedouZicmus","field":"[yenchesKecos]","value":"Yepe_Zermi"},
-{"box":"zedouZicmus","field":"[yenchesKecos]","value":"Conventional_Leust"},
-{"box":"zedouZicmus","field":"[yenchesKecos]","value":"Ouf_Bid"},
-{"box":"zedouZicmus","field":"[yenchesKecos]","value":"Ouf_Zermi"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"mifa"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"lopesaUc"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"yoc-youvoinetour"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"yoc-zen"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"yoc-zarbizermi"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"ss-lopesaUc"},
-{"box":"venereKeusseLancebaLassedeg","field":"[tiequar]","value":"ss-yoc"},
-{"box":"zomblou","field":"yeuf","value":"post kecla statistics and zarbizermi"},
-{"box":"zomblou","field":"yeuf","value":"pre kecla zen"},
-{"box":"zomblou","field":"yeuf","value":"pre kecla limits"},
-{"box":"zomblou","field":"yeuf","value":"pre kecla orders"},
-{"box":"lassedegLeurdit","field":"yeuf","value":"post kecla statistics and zarbizermi"},
-{"box":"lassedegLeurdit","field":"yeuf","value":"pre kecla zen"},
-{"box":"lassedegLeurdit","field":"yeuf","value":"pre kecla limits"},
-{"box":"lassedegLeurdit","field":"yeuf","value":"pre kecla orders"},
-{"box":"zomblou","field":"[mifaOideOiteOe]","value":"mifa"},
-{"box":"zomblou","field":"[mifaOideOiteOe]","value":"yoc-zen"},
-{"box":"zomblou","field":"[mifaOideOiteOe]","value":"yoc-ordernetour"},
-{"box":"zomblou","field":"[mifaOideOiteOe]","value":"yoc-zarbizermi"},
-{"box":"zomblou","field":"[mifaOideOiteOe]","value":"lopesaUc"},
-{"box":"zomblou","field":"[zessegonNeuthu]","value":"ss-yoc"},
-{"box":"zomblou","field":"[zessegonNeuthu]","value":"ss-lopesaUc"},
-{"box":"lassedegLeurdit","field":"oinje","value":"mifa"},
-{"box":"lassedegLeurdit","field":"oinje","value":"yoc-zen"},
-{"box":"lassedegLeurdit","field":"oinje","value":"yoc-youvoinetour"},
-{"box":"lassedegLeurdit","field":"oinje","value":"yoc-ordernetour"},
-{"box":"lassedegLeurdit","field":"oinje","value":"yoc-zarbizermi"},
-{"box":"lassedegLeurdit","field":"oinje","value":"lopesaUc"},
-{"box":"youvoi","field":"name","value":"par-lux-mifa-oinje"},
-{"box":"youvoi","field":"name","value":"par-lux-yoc-zen"},
-{"box":"youvoi","field":"name","value":"par-lux-yoc-zarbizermi"},
-{"box":"youvoi","field":"name","value":"par-lux-yoc-oinje-youvoi"},
-{"box":"youvoi","field":"name","value":"par-lux-yoc-oinje-orders"},
-{"box":"youvoi","field":"name","value":"par-lux-snap-yoc"},
-{"box":"youvoi","field":"name","value":"par-lux-std-oinje"},
-{"box":"usage","field":"{youvois}","value":"mifa:par-lux-mifa-oinje"},
-{"box":"usage","field":"{youvois}","value":"yoc-zen:par-lux-yoc-zen"},
-{"box":"usage","field":"{youvois}","value":"yoc-youvoinetour:par-lux-yoc-oinje-youvoi"},
-{"box":"usage","field":"{youvois}","value":"yoc-ordernetour:par-lux-yoc-oinje-orders"},
-{"box":"usage","field":"{youvois}","value":"yoc-zarbizermi:par-lux-yoc-zarbizermi"},
-{"box":"usage","field":"{youvois}","value":"lopesaUc:par-lux-std-oinje"}],
-"boxComments":[],
-"fieldComments":[
-{"box":"Lassedege","field":"al","comment":"we want to connect UTP as MDG youvoi. To achieve this,\\nMDG creates a server keusse using this (al, zeto).\\nUTP then creates a client keusse which requests a TCP/IP\\nconnection to the MDG server keusse."},
-{"box":"Lassedege","field":"zeto","comment":"we want to connect UTP as MDG youvoi. To achieve this,\\nMDG creates a server keusse using this (al, zeto).\\nUTP then creates a client keusse which requests a TCP/IP\\nconnection to the MDG server keusse."},
-{"box":"vessause","field":"lassedeg","comment":"Kafka lassedeg to which yeufiyech will be persisted\\nEach larfeuilleou actor given in configuration works only\\nwith a given Topic from our standpoint, so we just ask the\\nlassedeg here (but we really kisde a Topic/lassedeg pair).\\nWe have to see how the donbi is chosen. We expect no variability\\nat all, however, we may have topics such as: \\nQATECH.MDGOUT / EUA.MDGOUT / PROD.MDGOUT \\nif the Kafka brokers are shared. We have to see with respect to\\ndeployment machinery. Philippe Planchon says: itÂs not necessary\\nbut it could make sense to ensure that a EUA producer cannot easily\\npublish data mistakenly to productionÂ\\nCurrently the donbi (without prefix or suffix) for us would be something\\nlike: MDGOUT, MDGIN, MDSPY [maybe MDGOUT-HEARTBEAT + MDSPY-HEARTBEAT\\nhave to check with Benoit Compte."},
-{"box":"keusse","field":"al","comment":"These parameters are imposed by the business, they do not change with\\nthe emitting machine. The IP will be a multicast group IP. "},
-{"box":"keusse","field":"zeto","comment":"These parameters are imposed by the business, they do not change with\\nthe emitting machine. The IP will be a multicast group IP. "},
-{"box":"mefu","field":"zeberyep","comment":"zeberyep here is the name of the network card zeberyep used to emit\\npacket toward the given mefu (first mefu is mefu A, second mefu B)"},
-{"box":"mefu","field":"youvoiUc","comment":"youvoiUc is not mandatory, it would be to double check that the given\\nzeberyep really owns this IP"},
-{"box":"usage","field":"{youvois}","comment":"oinje to youvoi mapping."}],
-"links":[
-{"from":2,"fromField":1,"fromCardinality":"","to":14,"toField":1,"toCardinality":"","Category":""},
-{"from":2,"fromField":3,"fromCardinality":"","to":5,"toField":1,"toCardinality":"","Category":""},
-{"from":17,"fromField":2,"fromCardinality":"","to":7,"toField":1,"toCardinality":"","Category":""},
-{"from":6,"fromField":3,"fromCardinality":"","to":14,"toField":1,"toCardinality":"","Category":""},
-{"from":6,"fromField":6,"fromCardinality":"","to":5,"toField":1,"toCardinality":"","Category":""},
-{"from":1,"fromField":1,"fromCardinality":"","to":14,"toField":1,"toCardinality":"","Category":""},
-{"from":3,"fromField":1,"fromCardinality":"","to":4,"toField":0,"toCardinality":"","Category":""},
-{"from":5,"fromField":2,"fromCardinality":"","to":17,"toField":0,"toCardinality":"","Category":""},
-{"from":8,"fromField":0,"fromCardinality":"","to":9,"toField":3,"toCardinality":"","Category":""},
-{"from":14,"fromField":2,"fromCardinality":"","to":7,"toField":1,"toCardinality":"","Category":""},
-{"from":7,"fromField":1,"fromCardinality":"","to":0,"toField":0,"toCardinality":"","Category":""},
-{"from":7,"fromField":2,"fromCardinality":"","to":16,"toField":1,"toCardinality":"","Category":""},
-{"from":12,"fromField":1,"fromCardinality":"","to":14,"toField":1,"toCardinality":"","Category":""},
-{"from":10,"fromField":0,"fromCardinality":"","to":18,"toField":0,"toCardinality":"","Category":""},
-{"from":10,"fromField":2,"fromCardinality":"","to":16,"toField":1,"toCardinality":"","Category":""},
-{"from":10,"fromField":3,"fromCardinality":"","to":9,"toField":3,"toCardinality":"","Category":""},
-{"from":19,"fromField":0,"fromCardinality":"","to":10,"toField":1,"toCardinality":"","Category":""},
-{"from":19,"fromField":2,"fromCardinality":"","to":7,"toField":1,"toCardinality":"","Category":""},
-{"from":19,"fromField":3,"fromCardinality":"","to":8,"toField":2,"toCardinality":"","Category":""},
-{"from":19,"fromField":1,"fromCardinality":"","to":11,"toField":2,"toCardinality":"","Category":""},
-{"from":16,"fromField":1,"fromCardinality":"","to":3,"toField":1,"toCardinality":"","Category":""},
-{"from":11,"fromField":0,"fromCardinality":"","to":13,"toField":0,"toCardinality":"","Category":""},
-{"from":9,"fromField":0,"fromCardinality":"","to":15,"toField":0,"toCardinality":"","Category":""}],
-"fieldColors":[],
-"pictures":[]}
+INSERT INTO diagram(iddiagram, title) VALUES (1, 'Jacket Gafa BlemePro');
+
+INSERT INTO box(idbox, title, iddiagram) VALUES (1, 'ChanmeBlemeproPec', 1);
+INSERT INTO field(name, idbox) VALUES ('teillebouRap', 1);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES (2, 'PouilledePotecaCaillerag', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 2), ('reusta', 2);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(3, 'Blemepro', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 3), ('Peuclo', 3), ('name', 3), ('phoneTel', 3), ('donbi', 3);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(4, 'TassepeTcheubi', 1);
+INSERT INTO field(name, idbox) VALUES ('telmor', 4), ('meuf', 4), ('guedinTeub', 4), ('teillebouRap', 4), ('guezmere', 4), ('donbi', 4);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(5, 'GuedraTeucheTipeu', 1);
+INSERT INTO field(name, idbox) VALUES ('tepo', 5), ('teillebouRap', 5);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(6, 'TejeTiepeTigenToncarInsseGueusGuedinUc', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 6), ('yeuf', 6), ('KeneVeugra', 6);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(7, 'Lassedege', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 7), ('al', 7), ('zessegon', 7), ('Peuclo', 7), ('yepe', 7), ('zeto', 7), ('phoneTel', 7);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(8, 'fechnoueuf', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 8), ('gova', 8), ('vessause', 8);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(9, 'ZetiNoichNeuthuZincMeucaYoce', 1);
+INSERT INTO field(name, idbox) VALUES ('[lassedeg]', 9), ('[yecous]', 9), ('zebe', 9);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(10, 'zomblou', 1);
+INSERT INTO field(name, idbox) VALUES ('[zedouZicmus]', 10), ('[mifaOideOiteOe]', 10), ('[zessegonNeuthu]', 10), ('yeuf', 10);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(11, 'lassedegLeurdit', 1);
+INSERT INTO field(name, idbox) VALUES ('[zomblou]{2}', 11), ('zenZincs', 11), ('vessause', 11), ('yeuf', 11), ('oinje', 11);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(12, 'youvoi', 1);
+INSERT INTO field(name, idbox) VALUES ('[yeufi]{2}', 12), ('zyva', 12), ('yepe', 12), ('zetyenchesZeyo', 12), ('nemobitKps', 12), ('keuf', 12);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(13, 'lacelles', 1);
+INSERT INTO field(name, idbox) VALUES ('keblo', 13), ('euf', 13);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(14, 'mefu', 1);
+INSERT INTO field(name, idbox) VALUES ('cesu', 14), ('zeberyep', 14), ('youvoiUc', 14);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(15, 'euf', 1);
+INSERT INTO field(name, idbox) VALUES ('lassedeg', 15), ('id', 15), ('fichateraToDeuspidrepo', 15), ('yecous', 15);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(16, 'zedouZicmus', 1);
+INSERT INTO field(name, idbox) VALUES ('[updatesKecos]', 16), ('yeufiyechKeco', 16);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(17, 'vessause', 1);
+INSERT INTO field(name, idbox) VALUES ('lassedeg', 17), ('persistenceYeche', 17);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(18, 'KeneVeugra', 1);
+INSERT INTO field(name, idbox) VALUES ('name', 18), ('[symbolsIndices]', 18), ('kisdeAggregator', 18);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(19, 'keusse', 1);
+INSERT INTO field(name, idbox) VALUES ('al', 19), ('zeto', 19);
+
+INSERT INTO box(idbox, title, iddiagram) VALUES(20, 'usage', 1);
+INSERT INTO field(name, idbox) VALUES ('[zomblouLeurdits]', 20), ('{youvois}', 20), ('fechnoueuf', 20), ('lancebaVeucheYeuveZarbiZeu', 20), ('Zeyos', 20), ('euf', 20);
+
+INSERT INTO link(idbox_from, idfield_from, idbox_to, idfield_to) VALUES
+(2+1, 1+1, 14+1, 1+1),
+(2+1, 3+1, 5+1, 1+1),
+(17+1, 2+1, 7+1, 1+1),
+(6+1, 3+1, 14+1, 1+1),
+(6+1, 6+1, 5+1, 1+1),
+(1+1, 1+1,  14+1,  1+1),
+(3+1, 1+1,  4+1,  0+1),
+(5+1, 2+1,  17+1,  0+1),
+(8+1, 0+1,  9+1,  3+1),
+(14+1, 2+1,  7+1,  1+1),
+(7+1, 1+1,  0+1,  0+1),
+(7+1, 2+1,  16+1,  1+1),
+(12+1, 1+1,  14+1,  1+1),
+(10+1, 0+1,  18+1,  0+1),
+(10+1, 2+1,  16+1,  1+1),
+(10+1, 3+1,  9+1,  3+1),
+(19+1, 0+1,  10+1,  1+1),
+(19+1, 2+1,  7+1,  1+1),
+(19+1, 3+1,  8+1,  2+1),
+(19+1, 1+1,  11+1,  2+1),
+(16+1, 1+1,  3+1,  1+1),
+(11+1, 0+1,  13+1,  0+1),
+(9+1, 0+1,  15+1,  0+1);
