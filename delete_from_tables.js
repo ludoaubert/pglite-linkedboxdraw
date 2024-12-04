77f46e39a2e7958de53dba6f8b91a7994c642edc
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-04 11:18:49 +0100

    Update delete_from_tables.js

--- delete_from_tables.js
+++ delete_from_tables.js
@@ -13,15 +13,15 @@ DELETE FROM field;
 DELETE FROM box;
 DELETE FROM diagram;
 
-ALTER SEQUENCE polyline_idpolyline_seq RESTART WITH 1
-ALTER SEQUENCE translation_idtranslation_seq RESTART WITH 1
-ALTER SEQUENCE rectangle_idrectangle_seq RESTART WITH 1
-ALTER SEQUENCE graph_idgraph_seq RESTART WITH 1
-ALTER SEQUENCE message_tag_idmessage_seq RESTART WITH 1
-ALTER SEQUENCE tag_idtag_seq RESTART WITH 1
-ALTER SEQUENCE link_idlink_seq RESTART WITH 1
-ALTER SEQUENCE value_idvalue_seq RESTART WITH 1
-ALTER SEQUENCE field_idfield_seq RESTART WITH 1
-ALTER SEQUENCE box_idbox_seq RESTART WITH 1
-ALTER SEQUENCE diagram_iddiagram_seq RESTART WITH 1
+ALTER SEQUENCE polyline_idpolyline_seq RESTART WITH 1;
+ALTER SEQUENCE translation_idtranslation_seq RESTART WITH 1;
+ALTER SEQUENCE rectangle_idrectangle_seq RESTART WITH 1;
+ALTER SEQUENCE graph_idgraph_seq RESTART WITH 1;
+ALTER SEQUENCE message_tag_idmessage_seq RESTART WITH 1;
+ALTER SEQUENCE tag_idtag_seq RESTART WITH 1;
+ALTER SEQUENCE link_idlink_seq RESTART WITH 1;
+ALTER SEQUENCE value_idvalue_seq RESTART WITH 1;
+ALTER SEQUENCE field_idfield_seq RESTART WITH 1;
+ALTER SEQUENCE box_idbox_seq RESTART WITH 1;
+ALTER SEQUENCE diagram_iddiagram_seq RESTART WITH 1;
 `
