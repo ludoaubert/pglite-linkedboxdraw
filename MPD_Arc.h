667d05c42f4b391cd9d5160a7d7e66751db07c85
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-10 17:18:15 +0100

    Add files via upload

--- MPD_Arc.h
+++ MPD_Arc.h
@@ -9,13 +9,20 @@
 #define _MPD_ARC_
 
 
+#include <vector>
+
+
 struct MPD_Arc
 {
 	int _i ;
 	int _j ;
-
-	bool operator==(const MPD_Arc&) const = default;
 } ;
+
+bool operator==(const MPD_Arc& a, const MPD_Arc& b) ;
+
+std::vector<const MPD_Arc*> list_edges(const std::vector<std::vector<MPD_Arc> >& graph) ;
+std::vector<MPD_Arc*> list_edges(std::vector<std::vector<MPD_Arc> >& graph) ;
+std::vector<MPD_Arc> list_edges_(const std::vector<std::vector<MPD_Arc> >& graph) ;
 
 
 #endif
