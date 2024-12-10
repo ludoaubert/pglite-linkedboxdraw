e86c6db507dc787ac500c44d416a385f90d166bc
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-10 20:04:05 +0100

    Update diagram_allocation.cpp

--- diagram_allocation.cpp
+++ diagram_allocation.cpp
@@ -296,7 +296,7 @@ const char* diagram_allocation(int n, //nb boxes
 	struct Context
 	{
 		vector<int> nodes;
-		vector<vector<int> > adjacency_list;
+		vector<vector<MPD_Arc> > adjacency_list;
 	};
 
 	vector<Context> contexts;
