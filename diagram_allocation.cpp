8b0aa8940cd2debaf1d809da58b83bfe7890183c
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-10 17:41:48 +0100

    Update diagram_allocation.cpp

--- diagram_allocation.cpp
+++ diagram_allocation.cpp
@@ -449,6 +449,6 @@ Linux command to lookup eigen3 directory:
 
 
 To generate diagram_allocation.wasm and diagram_allocation.js:
-emcc diagram_allocation.cpp permutation.cpp KMeansRexCore..cpp MPD_Arc.cpp -o diagram_allocation.js -I/usr/include/eigen3 -Wno-c++11-narrowing -s EXPORTED_FUNCTIONS='["allocate_diagrams"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s ALLOW_MEMORY_GROWTH=1  -s EXPORT_ES6=1 -s MODULARIZE=1 -s EXPORT_NAME="createAllocationModule"  -s TOTAL_STACK=32MB
+emcc diagram_allocation.cpp permutation.cpp KMeansRexCore..cpp MPD_Arc.cpp -o diagram_allocation.js -I/usr/include/eigen3 -Wno-c++11-narrowing -s EXPORTED_FUNCTIONS='["allocate_diagrams"]' -s EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' -s ALLOW_MEMORY_GROWTH=1  -s EXPORT_ES6=1 -s MODULARIZE=1 -s EXPORT_NAME="createAllocationModule"  -s TOTAL_STACK=32MB  -std=c++20
 
 */
