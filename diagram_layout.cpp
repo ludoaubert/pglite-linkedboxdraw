10d967e41485a3c3c1ec6ab42808e88dd0f36846
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-11 14:52:40 +0100

    Update diagram_layout.cpp

--- diagram_layout.cpp
+++ diagram_layout.cpp
@@ -713,22 +713,29 @@ void stair_steps_layout(vector<MyRect> &vect, const vector<vector<MPD_Arc> > &ad
 
 //interface for emscripten wasm
 extern "C" {
-const char* diagram_layout(int n, //nb boxes
-                      int edge_count,
-                      const char* sedges)
+const char* diagram_layout(int rect_border,
+			const char* srects,
+			const char* sedges)
 {
-	vector<int> nodes(n);
-	ranges::copy(views::iota(0,n), nodes.begin());
+	vector<MyRect> rectangles;
+	int pos, nn;
+
+	pos = 0;
+    	MyRect r{0,0,0,0};
+	while (sscanf(srects + pos, "%3hx%3hx%3hx%3hx%n", &r.m_left, &r.m_right, &r.m_top, &r.m_bottom, &nn) == 2)
+	{
+	//use variable 'MyRect.no_sequence' to keep the original position of the box.
+	//keep in mind that variable 'MyRect.i' is used internally by some algorithms and cannot be used for that purpose.
+		r.no_sequence = r.i = rectangles.size();
+		rectangles.push_back(r);
+		pos += nn;
+	}
 	
         vector<MPD_Arc> edges;
-        int pos = 0;
-	int nn=0;
+        pos = 0;
 	MPD_Arc edge;
-	while (edges.size() < edge_count &&
-	    sscanf(sedges + pos, "%3x%3x%n", &edge._i, &edge._j, &nn) == 2)
+	while (sscanf(sedges + pos, "%3x%3x%n", &edge._i, &edge._j, &nn) == 2)
 	{
-                assert(edge._i < n);
-                assert(edge._j < n);
 		edges.push_back(edge);
 		pos += nn;
 	}
@@ -736,25 +743,25 @@ const char* diagram_layout(int n, //nb boxes
 	printf("edges.size()=%zu\n", edges.size());
 	printf("edge_count=%d\n", edge_count);
 
+	int n=rectangles.size();
 
-	string jsonPermutation1 = JSON_stringify(permutation1);
-	printf("permutation1=%s\n", jsonPermutation1.c_str());
+	vector<vector<MPD_Arc> > adjacency_list(n);
+	for (MPD_Arc &edge : edges)
+		adjacency_list[edge._i].push_back(edge);
 
+	stair_steps_layout(rectangles, adjacency_list, rect_border);
 
 	int printpos=0;
 	static char buffer[100000];
 
 	printpos += sprintf(buffer + printpos, "[\n");
 
-	for (int contextIndex=0; contextIndex < contexts.size(); contextIndex++)
+	for (const MyRect& r : rectangles)
 	{
-		const Context& ctx = contexts[contextIndex];
-		for (const int& i : ctx.nodes)
-		{
-			printpos += sprintf(buffer + printpos, "{\"id\":%d, \"context\":%d}%c\n", i, contextIndex,
-                              &i == &ctx.nodes.back() ? ' ' : ',');
-		}
+		printpos += sprintf(buffer + printpos, "{\"id\":%d, \"x\":%d, \"y\":%d}%c\n", i, r.m_left, r.m_top,
+                          &r == &rectangles.back() ? ' ' : ',');
 	}
+
 	printpos += sprintf(buffer + printpos, "]\n");
 	
 	return buffer;
@@ -763,12 +770,14 @@ const char* diagram_layout(int n, //nb boxes
 
 int main(int argc, char* argv[])
 {
+/*
 	const int n = 68;  //nb boxes
 	const int max_nb_boxes_per_diagram = 20;
 	const int edge_count = 60;
 	const char* sedges = "00200700302300301b00400500502300600200803c00801500901b00a01b00b01300c02300d01b00e00f00f01301001101101b01302301402301501801902301901b01a01b01b03701b02c01c01b01e01b02001b02101102202602201102402302502802602302702302802402902302a02302b02302f02303001a03102803200503303203302803403503403003601b03703803803703903703902303b03703d00803e03c03f01b040013041043041008043023";
 	const char* jsonLayout = diagram_layout(n, max_nb_boxes_per_diagram, edge_count, sedges);
 	printf("%s", jsonAllocation);
+*/
 }
 
 /*
