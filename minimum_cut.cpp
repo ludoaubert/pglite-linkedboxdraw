3be0e21ca9acb821f66234177e5f3d565c236ed1
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-19 13:54:30 +0100

    Update minimum_cut.cpp

--- minimum_cut.cpp
+++ minimum_cut.cpp
@@ -46,41 +46,6 @@ vector<vector<MPD_Arc> > compute_adjacency_list(const MatrixXd& OW)
 	return adjacency_list ;
 }
 
-
-
-//must be computed from unoriented graph
-void connected_components(const vector<vector<MPD_Arc> >& adjacency_list,
-			  vector<int>& connected_component)
-{
-	int n = adjacency_list.size() ;
-	vector<bool> visited(n, false) ;
-	int comp=0 ;
-	for (int i=0; i < adjacency_list.size(); i++)
-	{
-		if (visited[i])
-			continue ;
-		stack<int> Q ;
-		Q.push(i) ;
-		while (!Q.empty())
-		{
-			int ii = Q.top() ;
-			Q.pop() ;
-			if (visited[ii])
-				continue ;
-			connected_component[ii] = comp ;
-			visited[ii] = true ;
-
-			for (int k=0; k < adjacency_list[ii].size(); k++)
-			{
-				int j=adjacency_list[ii][k]._j ;
-				if (!visited[j])
-					Q.push(j) ;
-			}
-		}
-		comp++ ;
-	}
-}
-
 string JSON_stringify(const vector<int>& v)
 {
 	char buffer[1000];
