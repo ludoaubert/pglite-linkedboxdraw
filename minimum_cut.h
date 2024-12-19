dafbc2497e1311bb9d031ec4fde031c597cdd7cc
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-19 13:53:12 +0100

    Update minimum_cut.h

--- minimum_cut.h
+++ minimum_cut.h
@@ -20,7 +20,3 @@ std::string JSON_stringify(const std::vector<int>& v);
 std::string JSON_stringify(const std::vector<double>& v);
 
 void rec_minimum_cut(const std::string &chemin);
-
-//must be computed from unoriented graph
-void connected_components(const std::vector<std::vector<MPD_Arc> >& adjacency_list,
-			  std::vector<int>& connected_component);
