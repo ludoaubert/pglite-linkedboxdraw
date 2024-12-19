d3374b988f080a04938753fdba59bace6e14e900
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-19 14:02:09 +0100

    Update minimum_cut.h

--- minimum_cut.h
+++ minimum_cut.h
@@ -14,9 +14,4 @@ extern std::vector<MPD_Arc> edges ;
 extern std::vector<NodeAllocation> allocation;
 extern int max_nb_boxes_per_diagram;
 
-
-std::string JSON_stringify(const std::vector<int>& v);
-
-std::string JSON_stringify(const std::vector<double>& v);
-
 void rec_minimum_cut(const std::string &chemin);
