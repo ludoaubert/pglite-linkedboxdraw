6dfe437f64b060583cbcc9c50aa32dd754281661
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-20 22:29:51 +0100

    Update diagram_layout.cpp

--- diagram_layout.cpp
+++ diagram_layout.cpp
@@ -248,7 +248,7 @@ bool stair_steps_(int rect_border, vector<MyRect> &rectangles, const vector<vect
 		printf("Line %d. %d are not selected.\n", __LINE__, nr);
 	}
 
-	for (int f=2; f<=4; f++)
+	for (int f=2; f<=6; f++)
 	{
 		printf("f=%d\n", f);
 		fflush(stdout);
