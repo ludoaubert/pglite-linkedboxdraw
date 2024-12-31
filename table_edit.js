e05137e48db8114f3e397c19d67a60eb2bf77646
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-31 20:57:29 +0100

    Update table_edit.js

--- table_edit.js
+++ table_edit.js
@@ -182,37 +182,9 @@ async function init() {
 				'polyline', (SELECT json_agg(row_to_json(polyline))::json FROM polyline)
 			);
    		`);
-/*		
-		const ret = await db.query(`
-  			WITH cte(table_name, json_table) AS (
-				SELECT 'diagram', json_agg(row_to_json(diagram))::json FROM diagram
-				UNION ALL
-				SELECT 'box', json_agg(row_to_json(box))::json FROM box
-				UNION ALL
-				SELECT 'field', json_agg(row_to_json(field))::json FROM field
-				UNION ALL
-				SELECT 'value', json_agg(row_to_json(value))::json FROM value
-				UNION ALL
-				SELECT 'link', json_agg(row_to_json(link))::json FROM link
-				UNION ALL
-				SELECT 'tag', json_agg(row_to_json(tag))::json FROM tag
-  				UNION ALL
-				SELECT 'message_tag', json_agg(row_to_json(message_tag))::json FROM message_tag
-				UNION ALL
-				SELECT 'graph', json_agg(row_to_json(graph))::json FROM graph
-				UNION ALL
-				SELECT 'rectangle', json_agg(row_to_json(rectangle))::json FROM rectangle
-				UNION ALL
-				SELECT 'translation', json_agg(row_to_json(translation))::json FROM translation
-				UNION ALL
-				SELECT 'polyline', json_agg(row_to_json(polyline))::json FROM polyline
-			)
-			SELECT json_agg(json_build_object('table_name',table_name,'json_table',COALESCE(json_table,'[]'::json))::text)
-			FROM cte;
-   		`);
-*/
+
 		const doc = ret.rows[0].json_build_object;
-		const json_doc = '[' + doc.join(',') + ']';
+		const json_doc = JSON.stringify(doc);
 		const response = await fetch(
 			//"https://www.diskloud.fr:3000/linkedboxdraw/post",
 			"https://192.168.0.21:8443/linkedboxdraw/post",
