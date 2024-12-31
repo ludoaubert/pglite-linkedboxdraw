233f83eca467ccbd47ef0bd53143995143e68376
Ludovic Aubert <ludo.aubert@gmail.com>
2024-12-31 10:06:32 +0100

    Update table_edit.js

--- table_edit.js
+++ table_edit.js
@@ -24,6 +24,7 @@ var currentColorBoxIndex = -1;
 var currentColorFieldIndex = -1;
 
 var input ;
+var upload;
 var editTitle ;
 var newDiagramButton ;
 var boxCombo ;
@@ -103,6 +104,7 @@ function newDiagram() {
 async function init() {
 
 	input = document.getElementById("fi");
+	upload = document.getElementById("upload_online_doc");
 	editTitle = document.getElementById("title");
 	newDiagramButton = document.getElementById("new diagram");
 	boxCombo = document.getElementById("boxes");
@@ -163,6 +165,37 @@ async function init() {
 		await db.exec(delete_from_tables);
 		await db.exec(diagData);
 		await data2contexts();
+	});
+	upload.addEventListener("change", async (evt)=>{
+		const ret = await db.query(`
+  			WITH cte(table_name, json_table) AS (
+				SELECT 'diagram', json_agg(row_to_json(diagram))::json FROM diagram
+				UNION ALL
+				SELECT 'box', json_agg(row_to_json(box))::json FROM box
+				UNION ALL
+				SELECT 'field', json_agg(row_to_json(field))::json FROM field
+				UNION ALL
+				SELECT 'value', json_agg(row_to_json(value))::json FROM value
+				UNION ALL
+				SELECT 'link', json_agg(row_to_json(link))::json FROM link
+				UNION ALL
+				SELECT 'tag', json_agg(row_to_json(tag))::json FROM tag
+  				UNION ALL
+				SELECT 'message_tag', json_agg(row_to_json(message_tag))::json FROM message_tag
+				UNION ALL
+				SELECT 'graph', json_agg(row_to_json(graph))::json FROM graph
+				UNION ALL
+				SELECT 'rectangle', json_agg(row_to_json(rectangle))::json FROM rectangle
+				UNION ALL
+				SELECT 'translation', json_agg(row_to_json(translation))::json FROM translation
+				UNION ALL
+				SELECT 'polyline', json_agg(row_to_json(polyline))::json FROM polyline
+			)
+			SELECT json_agg(json_build_object('table_name',table_name,'json_table',COALESCE(json_table,'[]'::json))::text)
+			FROM cte;
+   		`);
+		const doc = ret.rows[0].json_agg;
+		
 	});
 	editTitle.addEventListener("change", updateTitle);
 	newDiagramButton.addEventListener("click", async () => {await newDiagram(); await displayCurrent(); await drawDiag();});
