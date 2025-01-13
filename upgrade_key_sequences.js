e74968cc21e5388df2f88acc611891ec00cac13b
Ludovic Aubert <ludo.aubert@gmail.com>
2025-01-13 19:40:30 +0100

    Update upgrade_key_sequences.js

--- upgrade_key_sequences.js
+++ upgrade_key_sequences.js
@@ -1,3 +1,4 @@
+const upgrade_key_sequences=`
 SELECT setval(pg_get_serial_sequence('box', 'idbox'), (SELECT MAX(idbox) FROM box) + 1);
 SELECT setval(pg_get_serial_sequence('field', 'idfield'), (SELECT MAX(idfield) FROM field) + 1);
 SELECT setval(pg_get_serial_sequence('value', 'idvalue'), (SELECT MAX(idvalue) FROM value) + 1);
@@ -7,3 +8,4 @@ SELECT setval(pg_get_serial_sequence('link', 'idlink'), (SELECT MAX(idlink) FROM
 SELECT setval(pg_get_serial_sequence('graph', 'idgraph'), (SELECT MAX(idgraph) FROM graph) + 1);
 SELECT setval(pg_get_serial_sequence('message_tag', 'idmessage'), (SELECT MAX(idmessage) FROM message_tag) + 1);
 SELECT setval(pg_get_serial_sequence('polyline', 'idpolyline'), (SELECT MAX(idpolyline) FROM polyline) + 1);
+`;
